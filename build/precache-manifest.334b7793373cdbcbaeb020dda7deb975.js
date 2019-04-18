cda3f84f5e43e665152f3e10eaabb1335b525397
salmank97 <salman5@live.com>
2019-04-18 14:24:10 +0500

    app.js commented code deleted and other changes

---
+++ build/precache-manifest.334b7793373cdbcbaeb020dda7deb975.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "fcde0888376313478f87783fa90fd692",
+    "url": "/static/media/logo-blackweb.fcde0888.png"
+  },
+  {
+    "revision": "f96bc90c11b42bbee9d9b1191b857110",
+    "url": "/static/media/blockstack-icon.f96bc90c.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6769610cc962cc91eed5",
+    "url": "/static/js/main.6769610c.chunk.js"
+  },
+  {
+    "revision": "47664e7079d478f3e482",
+    "url": "/static/js/1.47664e70.chunk.js"
+  },
+  {
+    "revision": "6769610cc962cc91eed5",
+    "url": "/static/css/main.fac85a3e.chunk.css"
+  },
+  {
+    "revision": "47664e7079d478f3e482",
+    "url": "/static/css/1.07a7e4e2.chunk.css"
+  },
+  {
+    "revision": "b7e897ab33a24be3a766e2b6595d9b1b",
+    "url": "/index.html"
+  }
+];
